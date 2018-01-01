6c21f3f183ffd790eed9c7bbb999e88037e0662b
Lushui <718093910@qq.com>
2018-01-01 23:23:30 +0800

    add file

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 import VueQuillEditor from 'vue-quill-editor'
 // use
 Vue.use(VueQuillEditor)
-// axios.defaults.baseURL = 'http://192.168.1.110/tpnote/index.php/Home/'
+
 axios.defaults.baseURL = 'http://admin.lushui.space/index.php/Home/'
 Vue.prototype.$http = axios
 /* eslint-disable no-new */
